bf0492cb34a6db7baf78ee4b560cacbe5a7bc555
Dylan Ratcliffe <dylanratcliffe@outlook.com@>
2019-09-02 22:34:08 +0100

    Adding even more config. I really should also create a readme

---
+++ bin/steam_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the name of the Steam volume
+DISK=$(diskutil list | grep Steam | grep -oE '[^ ]+$')
+
+# Unmount it
+sudo diskutil umount $DISK
+
+# Mount it in the correct location
+sudo mount -t apfs $DISK /Users/dylan/Library/Application\ Support/Steam
+
+open -a Steam
